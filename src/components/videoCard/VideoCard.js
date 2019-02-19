5720b669135c40e242312a9d97a0b2b2aff62638
jonzy3000 <matt.r.jones1995@gmail.com>
2019-02-19 19:09:05 +0000

    Styles in same folder as component - component is responsible for import

--- src/components/videoCard/VideoCard.js
+++ src/components/videoCard/VideoCard.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Card } from "antd";
+import "./VideoCard.less";
 
 const VideoCard = ({ title, description, contentPartner, img }) => (
-  <Card className="video-card" bordered={false}>
-    <section className="video-header">
+  <Card className="card" bordered={false}>
+    <section className="header">
       <h1 className="title">{title}</h1>
       <p className="subtitle">By: {contentPartner}</p>
     </section>
-    <section className="video-preview">
+    <section className="preview">
       <img src={img} />
     </section>
     <section className="description">{description}</section>
